31e2b214b9fcaf14b8987c7aa23434a23ab0179d
Khamzat91 <khamzat91@mail.ru>
2022-10-17 21:06:33 +0300

    настройка route

--- mern-course/client/src/App.js
+++ mern-course/client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './index.css';
+import 'materialize-css';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
     <h1>Hello</h1>
     </div>
   );
