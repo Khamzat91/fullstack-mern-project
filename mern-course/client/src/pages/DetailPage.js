4b90c9bdd9715c93bdd4b34218e1eeb87ca763d6
Khamzat91 <khamzat91@mail.ru>
2022-10-25 00:17:39 +0300

    added DetailPage

--- mern-course/client/src/pages/DetailPage.js
+++ mern-course/client/src/pages/DetailPage.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useCallback, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { useHttp } from "../hooks/http.hook";
+import { AuthContext } from "../context/AuthContext";
+import Loader from "../components/Loader"
 
 export const DetailPage = () => {
-     
-    return (
-        <div>
-            <h1>Detail Page</h1>
-        </div>
-    );
+  const { token } = useContext(AuthContext);
+  const [link, setLink] = React.useState(null);
+  const { request, loading } = useHttp();
+  const linkId = useParams().id;
+
+  const getLink = useCallback(async () => {
+    try {
+     const fetched = await request(`/api/link/${linkId}`, "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLink(fetched)
+    } catch (e) {}
+  }, [token, linkId, request]);
+
+  React.useEffect(() => {
+   getLink() 
+  }, [getLink])
+
+  if (loading) {
+    return <Loader/>
+  }
+
+  return (
+    <div>
+      
+      {!loading && link && <LinkCard/>}
+    </div>
+  );
 };
