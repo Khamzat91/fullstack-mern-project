acdfcdd0dcde4a07150906d11de19be6b9028e24
Khamzat91 <khamzat91@mail.ru>
2022-10-31 23:47:09 +0300

    added LinkCard

--- mern-course/client/src/components/LinkCard.js
+++ mern-course/client/src/components/LinkCard.js
@@ -4,7 +4,6 @@ const LinkCard = ({link}) => {
     return (
         <div>
             <h2>Ссылка</h2>
-
             <p>Ваша ссылка: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
             <p>Откуда: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
             <p>Количество кликов по ссылке: <strong>{link.clicks}</strong></p>
